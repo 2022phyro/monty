bac07cf9b18c5744d73dd2a3a0129818a4867569
2022phyro <phyrokelstein2@gmail.com>
2022-12-27 14:39:48 +0100

    MAjor changes, moving test work from ada repo here

--- monty.h
+++ monty.h
@@ -1,17 +1,20 @@
-#ifndef __MONTY_H__
-#define __MONTY_H__
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-#define STACK 0
-#define QUEUE 1
-#define DELIMS " \n\t\a\b"
-
-/* GLOBAL OPCODE TOKENS */
-extern char **op_toks;
-
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#define NPTRS(n) (n * sizeof(void *))
+#define NCHARS(n) (n * sizeof(char))
+#define bool int
+#define true 1
+#define false 0
+#define STACK 1
+#define QUEUE 0
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -19,7 +22,7 @@ extern char **op_toks;
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -27,63 +30,71 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * struct ada - all my clobal variables
+ *
+ * @mode: the mode
+ * @result: the result
+ * @status: the status
+ * @string: the string
+ * @arguments: the arguments
+ */
+typedef struct ada
+{
+	int mode;
+	char **result;
+	char **arguments;
+	char *string;
+	int status;
+} a;
+/* GLOBAL VARIABLES */
+extern int mode;
+extern char **result;
+extern int status;
+extern char *string;
+extern char **arguments;
 
-/* PRIMARY INTERPRETER FUNCTIONS */
-void free_stack(stack_t **stack);
-int init_stack(stack_t **stack);
-int check_mode(stack_t *stack);
-void free_tokens(void);
-unsigned int token_arr_len(void);
-int run_monty(FILE *script_fd);
-void set_op_tok_error(int error_code);
+/* OPCODES */
+void op_push(stack_t **head, unsigned int line_no);
+void op_pall(stack_t **head, unsigned int line_no);
+void op_pint(stack_t **head, unsigned int line_no);
+void op_pop(stack_t **head, unsigned int line_no);
+void op_swap(stack_t **head, unsigned int line_no);
+void op_add(stack_t **head, unsigned int line_no);
+void op_nop(stack_t **head, unsigned int line_no);
+void op_sub(stack_t **head, unsigned int line_no);
+void op_div(stack_t **head, unsigned int line_no);
+void op_mul(stack_t **head, unsigned int line_no);
+void op_mod(stack_t **head, unsigned int line_no);
+void op_pchar(stack_t **head, unsigned int line_no);
+void op_pstr(stack_t **head, unsigned int line_no);
+void op_rotr(stack_t **head, unsigned int line_no);
+void op_rotl(stack_t **head, unsigned int line_no);
+void op_stack(stack_t **head, unsigned int line_no);
+void op_queue(stack_t **head, unsigned int line_no);
 
-/* OPCODE FUNCTIONS */
-void monty_push(stack_t **stack, unsigned int line_number);
-void monty_pall(stack_t **stack, unsigned int line_number);
-void monty_pint(stack_t **stack, unsigned int line_number);
-void monty_pop(stack_t **stack, unsigned int line_number);
-void monty_swap(stack_t **stack, unsigned int line_number);
-void monty_add(stack_t **stack, unsigned int line_number);
-void monty_nop(stack_t **stack, unsigned int line_number);
-void monty_sub(stack_t **stack, unsigned int line_number);
-void monty_div(stack_t **stack, unsigned int line_number);
-void monty_mul(stack_t **stack, unsigned int line_number);
-void monty_mod(stack_t **stack, unsigned int line_number);
-void monty_pchar(stack_t **stack, unsigned int line_number);
-void monty_pstr(stack_t **stack, unsigned int line_number);
-void monty_rotl(stack_t **stack, unsigned int line_number);
-void monty_rotr(stack_t **stack, unsigned int line_number);
-void monty_stack(stack_t **stack, unsigned int line_number);
-void monty_queue(stack_t **stack, unsigned int line_number);
+/* UTILITIES */
+bool appendStr(char ***arr, size_t *size, char *str, int index);
+char **split(char *string, char *delimiter, unsigned int max);
+void *_realloc(void *ptr, size_t osize, size_t nsize);
+char *pen(char *filename);
+void (*get_opcode(char *s))(stack_t **, unsigned int);
+void free_stack(stack_t *h);
+void cleanup(stack_t *h);
+int check(char *s);
+int checkatoi(char *s);
 
-/* CUSTOM STANDARD LIBRARY FUNCTIONS */
-char **strtow(char *str, char *delims);
-char *get_int(int n);
-
-/* ERROR MESSAGES & ERROR CODES */
-int usage_error(void);
-int malloc_error(void);
-int f_open_error(char *filename);
-int unknown_op_error(char *opcode, unsigned int line_number);
-int no_int_error(unsigned int line_number);
-int pop_error(unsigned int line_number);
-int pint_error(unsigned int line_number);
-int short_stack_error(unsigned int line_number, char *op);
-int div_error(unsigned int line_number);
-int pchar_error(unsigned int line_number, char *message);
-
-#endif /* __MONTY_H__ */
+#endif /*MONTY_H */
